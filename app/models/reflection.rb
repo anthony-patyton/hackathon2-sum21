0d4d75034a37c4dfe8492ece42199b76d341859f
Steph Mahnke <mahnkes1@msu.edu>
2021-07-10 10:20:53 -0600

    fixed validations

--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -2,5 +2,5 @@ class Reflection < ApplicationRecord
   belongs_to :tracker
 
   validates :summary, :did_right, :do_better, presence: true
-  validates :summary, :did_right, :do_better uniqueness: true
+  validates :summary, :did_right, :do_better, uniqueness: true
 end
